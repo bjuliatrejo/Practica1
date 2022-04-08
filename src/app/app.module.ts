7363125bdc30b5e261766902b9d23421acd5db97
UsielOM <usiel31197@outlook.es>
2022-04-07 21:45:53 -0500

    Practica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PdfmakeComponent } from './shared/pdfmake/pdfmake.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeModule } from './pages/homepages/home.module';
+import { NominaModule } from './pages/nomina/nomina.module';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PdfmakeComponent,
+    FooterComponent,
+ 
+   
+   
+   
+ 
   ],
   imports: [
     BrowserModule,
+    HomeModule,
+    NominaModule,
     AppRoutingModule
   ],
   providers: [],
